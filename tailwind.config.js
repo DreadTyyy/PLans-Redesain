6002dd1fcb522b534151ba13592ebd2b1751ef77
adib <adibhaidarzaky@gmail.com>
2024-11-08 10:00:01 +0700

    style leading, tracking, font gotham on desktop page & mobile only homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,18 @@ module.exports = {
         "primary-800": "#3A89FF",
         secondary: "",
         "dark-900": "#212121",
+        "dark-850": "#313131",
+        "dark-820": "#404040",
         "dark-800": "#848484",
+        "dark-750": "#828282",
         "dark-600": "#737373",
         "dark-500": "#7B7B7B",
+        "dark-200": "#D8D8D8",
+        "dark-50": "#BDBDBD",
+        "blue-900": "#12294A"
       },
       fontFamily: {
-        "my-gotham": "'Gotham'",
+        "plans-gotham-bold": "'GothamBold'",
       },
       backgroundImage: {
         "header-jumbotron": "url('/assets/header jumbotron.png')",
@@ -27,9 +33,24 @@ module.exports = {
         footer: "-1px 0 20px 6px rgb(0 0 0 / 0.08)"
       },
       fontSize: {
-        base: ["1rem", 1.7],
-        sm: ["14px", 1.2],
-      }
+        "plans-heading": ["1rem"],
+        // sm: ["14px"],
+      },
+      letterSpacing: {
+        '-6': '-0.06em', 
+        '-3': '-0.03em', 
+        '-2': '-0.02em', 
+      },
+      lineHeight: {
+        'plans-md': '3.2rem', // 51.2px
+        'plans-nmd': '3rem', // 48px
+        'plans-normal': '2.4rem', // 38.4px
+        'plans-sxl': '2rem', // 32px
+        'plans-smd': '1.8rem', // 28.8px
+        'plans-sm': '1.607rem', // 25.7px
+        'plans-xs': '1.4rem', // 22.4px
+        'plans-xsm': '1.043rem', // 16.7px
+      },
     },
   },
   plugins: [],
